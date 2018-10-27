fec94dd96619fbc5fd7e62eda7b68a692149b758
asafmor <captainmor@gmail.com>
2018-10-28 01:02:35 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,288 @@
+<template>
+<div id="app">
+<div class="container h-100">
+	<div class="row justify-content-center align-items-center">
+
+		<svg id="simon" preserveAspectRatio="xMidYMid meet" viewBox="0 0 640 640" width="80%" height="80%">
+			<defs>
+				<path d="M534.65 316.8C534.65 441.88 433.1 543.42 308.03 543.42C182.95 543.42 81.41 441.88 81.41 316.8C81.41 191.73 182.95 90.18 308.03 90.18C433.1 90.18 534.65 191.73 534.65 316.8Z" id="fUNWitc9x"/>
+				<path d="M531.44 316.8C531.44 440.11 431.33 540.22 308.03 540.22C184.72 540.22 84.61 440.11 84.61 316.8C84.61 193.49 184.72 93.38 308.03 93.38C431.33 93.38 531.44 193.49 531.44 316.8Z" id="e3rqTSyMc4"/>
+				<path d="M321.68 213.43C349.15 213.98 402.94 243.71 411.34 303.96C411.95 308.08 414.47 310.01 418.89 309.76L513.15 309.76C518.61 309.97 520.67 305.85 519.34 297.39C506.6 199.92 446.01 119.59 321.92 104.63C316.85 103.88 314.39 105.9 314.54 110.71C314.54 162.07 314.54 194.16 314.54 207C314.54 207 314.6 207.96 314.6 207.96C315.05 211.55 317.41 213.38 321.68 213.43Z" id="agv5yLrw9"/>
+				<path d="M321.68 213.43C349.15 213.98 402.94 243.71 411.34 303.96C411.95 308.08 414.47 310.01 418.89 309.76L513.15 309.76C518.61 309.97 520.67 305.85 519.34 297.39C506.6 199.92 446.01 119.59 321.92 104.63C316.85 103.88 314.39 105.9 314.54 110.71C314.54 162.07 314.54 194.16 314.54 207C314.54 207 314.6 207.96 314.6 207.96C315.05 211.55 317.41 213.38 321.68 213.43Z" id="a1HfjPdn2f"/>
+				<path d="M323.2 211.95C350.67 212.5 404.46 242.23 412.86 302.48C413.47 306.6 415.99 308.53 420.41 308.27L514.67 308.27C520.13 308.48 522.19 304.36 520.86 295.91C508.11 198.43 447.53 118.11 323.43 103.15C318.37 102.39 315.91 104.42 316.06 109.23C316.06 160.58 316.06 192.68 316.06 205.52C316.06 205.52 316.12 206.47 316.12 206.47C316.57 210.07 318.93 211.89 323.2 211.95Z" id="c2mz1nhnhG"/>
+				<radialGradient id="gradientab8qVsQKp" gradientUnits="userSpaceOnUse" cx="310.22" cy="314.92" dx="454.65" dy="157.11" r="213.92">
+					<stop style="stop-color: #ffffff;stop-opacity: 1" offset="0%"/>
+					<stop style="stop-color: #e00000;stop-opacity: 1" offset="100%"/>
+				</radialGradient>
+				<path d="M321.85 420.4C349.32 419.84 403.11 390.11 411.51 329.86C412.12 325.74 414.64 323.81 419.06 324.07L513.32 324.07C518.78 323.86 520.84 327.98 519.51 336.43C506.76 433.91 446.18 514.23 322.09 529.19C317.02 529.95 314.56 527.92 314.71 523.11C314.71 471.76 314.71 439.66 314.71 426.82C314.71 426.82 314.77 425.87 314.77 425.87C315.22 422.27 317.58 420.45 321.85 420.4Z" id="a1tepp1275"/>
+				<path d="M294.21 419.9C266.74 419.35 212.95 389.62 204.55 329.37C203.94 325.25 201.42 323.32 197 323.57L102.73 323.57C97.28 323.36 95.22 327.48 96.55 335.94C109.29 433.41 169.88 513.74 293.97 528.7C299.04 529.45 301.49 527.43 301.35 522.61C301.35 471.26 301.35 439.17 301.35 426.33C301.35 426.33 301.29 425.37 301.29 425.37C300.84 421.78 298.48 419.95 294.21 419.9Z" id="d21TjpS7EM"/>
+				<path d="M292.86 421.74C265.39 421.19 211.6 391.46 203.2 331.21C202.59 327.09 200.07 325.16 195.65 325.42L101.38 325.42C95.93 325.21 93.87 329.33 95.2 337.78C107.94 435.26 168.53 515.58 292.62 530.54C297.69 531.3 300.15 529.27 300 524.46C300 473.11 300 441.01 300 428.17C300 428.17 299.94 427.22 299.94 427.22C299.49 423.62 297.13 421.8 292.86 421.74Z" id="a9V6Vn32vU"/>
+				<radialGradient id="gradienta1Mn8PIBOG" gradientUnits="userSpaceOnUse" cx="308.03" cy="316.8" dx="168.32" dy="477.31" r="212.79">
+					<stop style="stop-color: #ffffff;stop-opacity: 1" offset="0%"/>
+					<stop style="stop-color: #ffd500;stop-opacity: 1" offset="100%"/>
+				</radialGradient>
+				<path d="M293.83 213.43C266.36 213.98 212.57 243.71 204.18 303.96C203.56 308.08 201.04 310.01 196.62 309.76L102.36 309.76C96.9 309.97 94.84 305.85 96.18 297.39C108.92 199.92 169.5 119.59 293.6 104.63C298.66 103.88 301.12 105.9 300.97 110.71C300.97 162.07 300.97 194.16 300.97 207C300.97 207 300.91 207.96 300.91 207.96C300.46 211.55 298.1 213.38 293.83 213.43Z" id="a5g4P9GEa"/>
+				<path d="M292.86 211.95C265.39 212.5 211.6 242.23 203.2 302.48C202.59 306.6 200.07 308.53 195.65 308.27L101.38 308.27C95.93 308.48 93.87 304.36 95.2 295.91C107.94 198.43 168.53 118.11 292.62 103.15C297.69 102.39 300.15 104.42 300 109.23C300 160.58 300 192.68 300 205.52C300 205.52 299.94 206.47 299.94 206.47C299.49 210.07 297.13 211.89 292.86 211.95Z" id="a9IMrUopw"/>
+				<radialGradient id="gradientb9gNPXX34" gradientUnits="userSpaceOnUse" cx="310.79" cy="325.4" dx="158.26" dy="161.52" r="223.88">
+					<stop style="stop-color: #ffffff;stop-opacity: 1" offset="0%"/>
+					<stop style="stop-color: #00c307;stop-opacity: 1" offset="100%"/>
+				</radialGradient>
+				<path d="M323.2 421.74C350.67 421.19 404.46 391.46 412.86 331.21C413.47 327.09 415.99 325.16 420.41 325.42L514.67 325.42C520.13 325.21 522.19 329.33 520.86 337.78C508.11 435.26 447.53 515.58 323.43 530.54C318.37 531.3 315.91 529.27 316.06 524.46C316.06 473.11 316.06 441.01 316.06 428.17C316.06 428.17 316.12 427.22 316.12 427.22C316.57 423.62 318.93 421.8 323.2 421.74Z" id="naWIn1awR"/>
+				<radialGradient id="gradienta2UogFVCj9" gradientUnits="userSpaceOnUse" cx="311.34" cy="316.8" dx="469.94" dy="453.68" r="209.51">
+					<stop style="stop-color: #ffffff;stop-opacity: 1" offset="0%"/>
+					<stop style="stop-color: #007ed7;stop-opacity: 1" offset="100%"/>
+				</radialGradient>
+				<path d="M346.01 353.79C346.01 359.7 341.21 364.5 335.29 364.5C329.38 364.5 324.57 359.7 324.57 353.79C324.57 347.87 329.38 343.07 335.29 343.07C341.21 343.07 346.01 347.87 346.01 353.79Z" id="i11HKExN"/>
+				<radialGradient id="gradientfayfFvOuH" gradientUnits="userSpaceOnUse" cx="335.29" cy="354.28" dx="335.29" dy="370.82" r="16.54">
+					<stop style="stop-color: #cff3c7;stop-opacity: 1" offset="0%"/>
+					<stop style="stop-color: #00cc00;stop-opacity: 1" offset="100%"/>
+				</radialGradient>
+				<path d="M294.41 340.82C295.34 340.82 296.09 341.57 296.09 342.5C296.09 347.31 296.09 360.04 296.09 364.85C296.09 365.78 295.34 366.54 294.41 366.54C289.76 366.54 277.5 366.54 272.86 366.54C271.93 366.54 271.17 365.78 271.17 364.85C271.17 360.04 271.17 347.31 271.17 342.5C271.17 341.57 271.93 340.82 272.86 340.82C277.5 340.82 289.76 340.82 294.41 340.82Z" id="bIzLsERYl"/>
+						<text id="gui1CQlxQ" x="284.23" y="364.5" font-size="12" font-family="Orbitron" font-weight="normal" font-style="normal" letter-spacing="0" alignment-baseline="before-edge" transform="matrix(1 0 0 1 -0.6020728451400714 -18.326759960665754)" style="line-height:100%" xml:space="preserve" dominant-baseline="text-before-edge">
+					<tspan x="284.23" dy="0em" alignment-baseline="before-edge" dominant-baseline="text-before-edge" text-anchor="middle">{{round}}</tspan>
+				</text>
+			</defs>
+			<g>
+				<g>
+					<g>
+						<use xlink:href="#fUNWitc9x" opacity="1" fill="#5f5f5f" fill-opacity="1"/>
+					</g>
+					<g>
+						<use xlink:href="#e3rqTSyMc4" opacity="1" fill="#262626" fill-opacity="1"/>
+					</g>
+					<g>
+						<use xlink:href="#agv5yLrw9" opacity="1" fill="#ffb6b6" fill-opacity="1"/>
+					</g>
+					<g>
+						<use xlink:href="#a1HfjPdn2f" opacity="1" fill="#ffc3c3" fill-opacity="1"/>
+					</g>
+					<g class="game-button" @mousedown="press('red')" :class="{'highlight': pressed.red}">
+						<use xlink:href="#c2mz1nhnhG" opacity="1" fill="url(#gradientab8qVsQKp)"/>
+					</g>
+					<g>
+						<use xlink:href="#a1tepp1275" opacity="1" fill="#bfe9ff" fill-opacity="1"/>
+					</g>
+					<g>
+						<use xlink:href="#d21TjpS7EM" opacity="1" fill="#fff6a3" fill-opacity="1"/>
+					</g>
+					<g class="game-button" @mousedown="press('blue')" :class="{'highlight': pressed.blue}">
+						<use xlink:href="#a9V6Vn32vU" opacity="1" fill="url(#gradienta1Mn8PIBOG)"/>
+					</g>
+					<g>
+						<use xlink:href="#a5g4P9GEa" opacity="1" fill="#affea9" fill-opacity="1"/>
+					</g>
+					<g class="game-button" @mousedown="press('green')" :class="{'highlight': pressed.green}" style="background-color: black;">
+						<use xlink:href="#a9IMrUopw" opacity="1" fill="url(#gradientb9gNPXX34)"/>
+					</g>
+					<g class="game-button" @mousedown="press('yellow')" :class="{'highlight': pressed.yellow}">
+						<use xlink:href="#naWIn1awR" opacity="1" fill="url(#gradienta2UogFVCj9)"/>
+					</g>
+					<g class="game-button" @click="startGame()">
+						<use xlink:href="#i11HKExN" opacity="1" fill="url(#gradientfayfFvOuH)"/>
+					</g>
+					<g>
+						<use xlink:href="#bIzLsERYl" opacity="1" fill="#000000" fill-opacity="1"/>
+					</g>
+					<g id="emO5G7gYG">
+						<use xlink:href="#gui1CQlxQ" opacity="1" fill="#ffffff" fill-opacity="1"/>
+					</g>
+				</g>
+			</g>
+		</svg>
+
+    </div>
+</div>
+</div>
+</template>
+
+<script>
+let soundsBasePath = "./assets/sounds/";
+let sounds = {
+  red: require(soundsBasePath + "red.mp3"),
+  blue: require(soundsBasePath + "blue.mp3"),
+  yellow: require(soundsBasePath + "yellow.mp3"),
+  green: require(soundsBasePath + "green.mp3"),
+  win: require(soundsBasePath + "applause.wav")
+};
+
+export default {
+  computed: {},
+  data() {
+    return {
+      round: 0,
+      maxRounds: 20,
+      player: [],
+      simon: [],
+      pressable: false,
+      pressed: {
+        red: false,
+        blue: false,
+        yellow: false,
+        green: false
+      },
+      delayBetweenRounds: 500,
+      delayBetweenFlashes: 200,
+      flashDuration: 500
+    };
+  },
+  computed: {
+    playable() {
+      return this.pressable;
+    }
+  },
+  methods: {
+    disablePlayer() {
+      this.pressable = false;
+    },
+    enablePlayer() {
+      this.pressable = true;
+    },
+    press(color) {
+      if (!this.playable) {
+        return;
+      }
+
+      this.disablePlayer();
+      this.player.push(color);
+      this.flashSingle(color).then(() => {
+        this.checkLastPress();
+      });
+    },
+    checkLastPress() {
+      let idx = this.player.length - 1;
+      if (this.player[idx] === this.simon[idx]) {
+        if (this.player.length === this.simon.length) {
+          this.winRound();
+        } else {
+          // let the player continue with his sequence
+          this.enablePlayer();
+        }
+      } else {
+        this.loseGame();
+      }
+    },
+    winRound() {
+      if (this.round === this.maxRounds) {
+        this.winGame();
+      } else {
+        setTimeout(() => {
+          this.startNextRound();
+        }, this.delayBetweenRounds);
+      }
+    },
+    winGame() {
+      this.round = "YOU WON!";
+      this.playSound(sounds.win);
+    },
+    loseGame() {
+      this.round = "YOU LOST!";
+    },
+    startNextRound() {
+      this.disablePlayer();
+      this.player = [];
+      this.round++;
+      this.simon.push(this.getRandomColor());
+      this.simonSay(this.simon).then(() => {
+        this.enablePlayer();
+      });
+    },
+    resetGame() {
+      this.disablePlayer();
+      this.round = 0;
+      this.player = [];
+      this.simon = [];
+    },
+    startGame() {
+      this.resetGame();
+      this.startNextRound();
+    },
+    getRandomColor() {
+      let colors = ["red", "blue", "yellow", "green"];
+      let rnd = Math.floor(Math.random() * colors.length);
+      return colors[rnd];
+    },
+    simonSay(sequence) {
+      return this.flashSequence(sequence, this.delayBetweenFlashes);
+    },
+    flashSequence(sequence, gapTime) {
+      gapTime;
+      return new Promise(resolve => {
+        if (sequence && sequence.length > 0) {
+          let color = sequence[0];
+          this.flashSingle(color).then(() => {
+            setTimeout(() => {
+              this.simonSay(sequence.slice(1)).then(() => resolve());
+            }, gapTime);
+          });
+        } else {
+          resolve();
+        }
+      });
+    },
+    flashSingle(color) {
+      return new Promise(resolve => {
+        this.pressed[color] = true;
+        this.playSound(sounds[color]);
+        setTimeout(() => {
+          this.pressed[color] = false;
+          resolve();
+        }, this.flashDuration);
+      });
+    },
+    playSound(sound) {
+      if (sound) {
+        var audio = new Audio(sound);
+        audio.play();
+      }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.buttons-panel {
+  display: grid;
+  grid-template-rows: 1fr 1fr;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 0.3vw;
+}
+
+.game-button {
+  cursor: pointer;
+  width: 200px;
+  height: 200px;
+  border-radius: 0.5rem;
+  border: 2px solid rgba(0, 0, 0, 0.1);
+
+  &.highlight {
+    box-shadow: inset 0 0 220px #ffffff;
+    border-color: rgba(255, 255, 255, 0.5);
+  }
+}
+
+#red {
+  background-color: red;
+}
+
+#blue {
+  background-color: blue;
+}
+
+#green {
+  background-color: green;
+}
+
+#yellow {
+  background-color: yellow;
+}
+
+.digital-display {
+  font-family: "Orbitron";
+  padding: 0.5em;
+  color: red;
+  background-color: black;
+  font-size: 2rem;
+  width: 405px;
+  border-radius: 0.5rem;
+}
+</style>
